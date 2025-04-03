258ae62ef8a4046f9df253d451e2bc7f9a7a99fd
Kartik21407 <Kartikshah21407@gmail.com>
2025-04-03 12:58:18 +0530

    Create first-unique-character-in-a-string.cpp

---
+++ first-unique-character-in-a-string.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int firstUniqChar(string s)
+    {
+        unordered_map<char, int> mpp;
+        for (int i = 0; i < s.size(); i++)
+        {
+            mpp[s[i]]++;
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (mpp[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    Solution s;
+    string str = "leetcode";
+    int result = s.firstUniqChar(str);
+    if (result != -1)
+    {
+        cout << "First unique character is at index: " << result << endl;
+    }
+    else
+    {
+        cout << "No unique character found." << endl;
+    }
+    return 0;
+}
